ac53a70f6c5454b41415c23578ebcd5ab89ce338
Jason Brown <cardoza0581@hotmail.com>
2018-12-12 18:23:58 -0600

    if committed it will change the font of the site

--- src/Screens/index.js
+++ src/Screens/index.js
@@ -1,4 +1,4 @@
 export { default as Blog } from './Blog';
-export { default as Home } from './Home';
 export { default as Contact } from './contact';
+export { default as Home } from './Home';
 export { default as Projects } from './Project';
